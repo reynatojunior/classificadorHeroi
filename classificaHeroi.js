621c31d052ca4e941c1eb255544b31549698bd48
Reynato Junior <45671354+reynatojunior@users.noreply.github.com>
2023-10-17 21:06:38 -0300

    Commit desafio heroi

---
+++ classificaHeroi.js
@@ -0,0 +1,32 @@
+let nome = Thor
+let xp = 1500
+let nivel
+
+if(xp<1000){
+    nivel = Ferro
+    print("O Heroi de nome " + nome + "esta no nivel de" + nivel )
+}
+else if(xp>1000 && xp<=2000){
+    nivel = Bronze
+    print("O Heroi de nome " + nome + "esta no nivel de" + nivel )
+}
+else if(xp>2000 && xp<=5000){
+    nivel = Prata
+    print("O Heroi de nome " + nome + "esta no nivel de" + nivel )
+}else if(xp>6000 && xp<=7000){
+    nivel = Ouro
+    print("O Heroi de nome " + nome + "esta no nivel de" + nivel )
+}else if(xp>7000 && xp<=8000){
+    nivel = Platina
+    print("O Heroi de nome " + nome + "esta no nivel de" + nivel )
+}else if(xp>8000 && xp<=9000){
+    nivel = Ascendente
+    print("O Heroi de nome " + nome + "esta no nivel de" + nivel )
+}else if(xp>9000 && xp<=10000){
+    nivel = Imortal
+    print("O Heroi de nome " + nome + "esta no nivel de" + nivel )
+}else if(xp>=10000){
+    nivel = Radiante
+    print("O Heroi de nome " + nome + "esta no nivel de" + nivel )
+}
+
